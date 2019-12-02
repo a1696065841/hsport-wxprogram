04d74b3aaf2f50f6b821c063c5ac252ee1873d2a
a1696065841 <710766710@qq.com>
2019-12-02 11:40:02 +0800

    foodimg

---
+++ src/main/java/com/hsport/wxprogram/util/YanZhenCode.java
@@ -0,0 +1,24 @@
+package com.hsport.wxprogram.util;
+
+import java.util.Random;
+
+public class YanZhenCode {
+ 
+    private final static int codeLength =5;
+    
+    /**
+     * @see 产生随机验证码
+     * @return 验证码字符串
+     */
+	  public static String getCode(){
+		  
+		 Random rand = new  Random();
+		 int  a ;
+		 String  result ="";
+		 for( int j =0; j<codeLength; j++ ){
+			 a = Math.abs( rand.nextInt()%9 );
+			 result += String.valueOf(a);
+		 }
+		 return  result;
+	  }
+}
