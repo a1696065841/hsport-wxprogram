f3b627ee859a1385c5cacfdfe9ee9aff13706e9f
JustDo <710766710@qq.com>
2019-12-04 15:30:58 +0800

    add README

---
+++ src/main/java/com/hsport/wxprogram/service/ICeyiceService.java
@@ -0,0 +1,16 @@
+package com.hsport.wxprogram.service;
+
+import com.hsport.wxprogram.domain.Ceyice;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author lhb
+ * @since 2019-11-29
+ */
+public interface ICeyiceService extends IService<Ceyice> {
+
+}
---
+++ src/main/java/com/hsport/wxprogram/service/impl/CeyiceServiceImpl.java
@@ -0,0 +1,20 @@
+package com.hsport.wxprogram.service.impl;
+
+import com.hsport.wxprogram.domain.Ceyice;
+import com.hsport.wxprogram.mapper.CeyiceMapper;
+import com.hsport.wxprogram.service.ICeyiceService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author lhb
+ * @since 2019-11-29
+ */
+@Service
+public class CeyiceServiceImpl extends ServiceImpl<CeyiceMapper, Ceyice> implements ICeyiceService {
+
+}
